7de4538f41f1c2f63eef1881419e69c6c2086986
Max Kalagur <kalagur11@gmail.com>
2016-11-13 00:17:25 +0200

    validation interface comment

--- app/Services/Validator/ValidatorInterface.php
+++ app/Services/Validator/ValidatorInterface.php
@@ -1,20 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vova
- * Date: 12.11.16
- * Time: 15:50
- */
 
 namespace App\Services\Validator;
 
 
 interface ValidatorInterface
 {
-    /**
-     * @param $validation_data  get rules validation
-     * @param $data  get data for validation
-     * @return mixed return false in the event of fail or true in the event of success
+    /** Validation of data according to rules: 'require'(element exists), 'email', 'string', 'numeric'
+     *
+     * Example:
+     * $data = ['name'=>'vova', 'email'=>'1kaa@mail.com', 'password'=>'sdadsa', 'message'=>'da asdwq wq', 'phone' =>'230992231']
+     * (new Validator())->validation([
+     *       'name'=>['require', 'string'],
+     *       'email'=>['require', 'email'],
+     *       'password'=>['require'],
+     *       'phone'=>['numeric'],
+     *       'message'=>['string']
+     *   ], $data );
+     *       
+     * @param $validation_data - Array of elements and rules to be validated. 
+     * @param $data - Array of elements to be validated
+     * @return mixed return if fail - Array of errors, if success - true
      */
     public function validation($validation_data, $data);
 }
