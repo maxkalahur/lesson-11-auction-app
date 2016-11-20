63e6a5a1498cd94bbcb834622f10f81ac02f6729
RomaVan <lasedwes@rambler.ru>
2016-11-21 00:05:51 +0200

    Add Fb button

--- app/Views/login.html.php
+++ app/Views/login.html.php
@@ -1,4 +1,3 @@
-
 <div class="container">
     <form action="/login" method="POST" id="contactform" role="form">
         <div class="form-group">
@@ -9,18 +8,18 @@
             <p><label for="password">Password</label></p>
             <input id="password" name="login[pass]" class="form-control" tabindex="2" type="password" required>
         </div>
-        <div class="container">        <input type="checkbox" name="login[rememberMe]"> remember me</div>
+        <div class="container">        <input type="checkbox" name="login[rememberMe]"> Remember me</div>
         <br>
+        <div class="container">
         <input type="submit" value="Log in" id="submit" class="btn btn-default"/>
+        </div>
+<!--        <div class="container col-md-3">-->
+                <a class="btn  btn-social btn-facebook" href="https://www.facebook.com/v2.8/dialog/oauth?client_id=377416752598790&redirect_uri=http://mysite.com/fb-callback">
+                    <span class="fa fa-facebook"></span> Sign in with Facebook
+                </a>
+<!--        </div>-->
+
+
     </form>
 </div>
-<a class="btn btn-block btn-social btn-twitter">
-    <span class="fa fa-twitter"></span> Sign in with Twitter
-</a>
-Or if you just want the icon button, use it like this:
 
-<a class="btn btn-social-icon btn-twitter">
-    <span class="fa fa-twitter"></span>
-</a>
-</body>
-</html>
