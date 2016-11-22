175b7dad73b720bc9f257c391c604751340c1c21
vova177 <1kaapaa@gmail.com>
2016-11-22 20:04:10 +0200

    fix lot view

--- app/Views/lot.html.php
+++ app/Views/lot.html.php
@@ -1,20 +1,9 @@
-<table class="table table-bordered">
 <?php
-if(isset($data)) {
-    if (!empty($data['category_lots'][0])) {
-        foreach ($data['category_lots'] as $value) {
-            echo "<tr><td align='center'><a href='/lot/product?lot_id=" . $value->id . "'>" . $value->name . "</a></td></tr>";
-        }
-    }
-}?>
-    <?php
-    if(isset($data)){
     if(!empty($data['lot_page'][0])) {
         foreach ($data['lot_page'] as $value){
             echo  $value->name . "<br>";
             echo  $value->description . "<br>";
         }
-    }
-}?>
-</table>
+}
+
 
