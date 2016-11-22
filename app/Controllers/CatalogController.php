bc582d65a76b221f27b9f9bdd61c3096e04a4588
vova177 <1kaapaa@gmail.com>
2016-11-22 20:03:50 +0200

    fix catalog

--- app/Controllers/CatalogController.php
+++ app/Controllers/CatalogController.php
@@ -4,28 +4,34 @@ namespace App\Controllers;
 
 use App\Controllers\Controller;
 use App\Models\Catalog;
+use App\Models\Lot;
 use App\Framework\View;
+use App\Services\Pagination\Pagination;
 
 class CatalogController extends Controller
 {
     public function index(){
-        $arr_obj=Catalog::getBYparametr('0', 'parent');
-        if(!empty($arr_obj) && is_array($arr_obj)) {
-          $arr=['parent_cat'=>$arr_obj];
+        $pag= new Pagination();
+        $parent_cat=Catalog::getByParent(0);
+        $parent_cat=is_array($parent_cat) ? $parent_cat : [$parent_cat];
+        $pag->set_limit(3);
+        var_dump($pag->load_page($pag->next_page()));
+        var_dump($pag->load_page($pag->prev_page()));
+        $all_lots=Lot::all();
+        $all_lots=is_array($all_lots) ? $all_lots : [$all_lots];
+        $arr=isset($_GET['Parent_id']) ? ['expand_cat'=>Catalog::getByParent($_GET['Parent_id'])] :
+            (isset($_GET['Category_id']) ? ['lotsByCategory'=>Lot::getByCategory_id($_GET['Category_id'])] : false);
+        if($arr){
+            if(isset($arr['expand_cat'])){
+                $arr=is_array($arr['expand_cat']) ? $arr['expand_cat'] : [$arr['expand_cat']];
+                $arr=["expand_cat"=>$arr, "parent_cat"=>$parent_cat, "all_lots"=>$all_lots];
+            } else{
+                $arr=is_array($arr['lotsByCategory']) ? $arr['lotsByCategory'] : [$arr['lotsByCategory']];
+                $arr=["lotsByCategory"=>$arr, "parent_cat"=>$parent_cat];
+            }
         }else{
-            $arr=['parent_cat'=>['0'=>$arr_obj]];
+            $arr=["parent_cat"=>$parent_cat, "all_lots"=>$all_lots];
         }
         View::show("catalog", $arr);
 }
-
-   public function showCategory(){
-       $arr_obj=Catalog::getBYparametr($_GET['catalog_id'], 'parent');
-       if(!empty($arr_obj)){
-           $arr=['expand_cat'=>$arr_obj];
-       }else{
-           $arr=['expand_cat'=>['0'=>$arr_obj]];
-       }
-       View::show("catalog", $arr);
-   }
-
 }
