4d65e7aefaea4a6b635368ddbe47453144d59957
vova177 <1kaapaa@gmail.com>
2016-11-22 20:02:43 +0200

    create pagination

---
+++ app/Services/Pagination/Pagination.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services\Pagination;
+
+class Pagination
+{
+    protected $curr_page;
+    protected $limit_lots;
+    public function __construct()
+    {
+        $this->curr_page=0;
+        $this->limit_lots=3;
+    }
+    public function set_limit($new_limit)
+    {
+        return $this->limit_lots=$new_limit;
+    }
+    public function next_page()
+    {
+        return $this->curr_page+$this->limit_lots;
+    }
+    public function prev_page()
+    {
+        return $this->curr_page-$this->limit_lots;
+    }
+    public  function load_page($new_page)
+    {
+        $this->curr_page=$new_page;
+        return $sql="SELECT * FROM `lots` LIMIT $this->curr_page, $this->limit_lots";
+    }
+}
