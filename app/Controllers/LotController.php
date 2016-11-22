1a33d5a4156e351e457ccfb1dc8474f4e8abea09
vova177 <1kaapaa@gmail.com>
2016-11-22 20:04:44 +0200

    fix lot controller

--- app/Controllers/LotController.php
+++ app/Controllers/LotController.php
@@ -10,27 +10,13 @@ use App\Framework\View;
 class LotController
 {
     public function showLot(){
-        View::show("lot",['lot'=>Lot::all()]);
+        if(isset($_GET['lot_id'])){
+            $lot = Lot::get($_GET['lot_id']);
+            $arr=is_array($lot) ? ['lot_page'=>$lot] : ['lot_page'=>['0'=>$lot]];
+            View::show("lot", $arr);
+            }
     }
     public function withCategory(){
-        $arr=[];
-        if(isset($_GET['category_id'])) {
-            $arr_obj = Lot::getBYparametr($_GET['category_id'], 'category_id');
-            if (!empty($arr_obj) && is_array($arr_obj)) {
-                $arr = ['category_lots' => $arr_obj];
-            } else {
-                $arr = ['category_lots' => ['0' => $arr_obj]];
-            }
-        }
-        if(isset($_GET['lot_id'])){
-            $arr_obj = Lot::getBYparametr($_GET['lot_id'], 'id');
-            $arr_scr=LotPhoto::getBYparametr($_GET['lot_id'], 'lot_id');
-            if (!empty($arr_obj) && is_array($arr_obj)) {
-                $arr = ['lot_page' => $arr_obj];
-            } else {
-                $arr = ['lot_page' => ['0' => $arr_obj]];
-            }
-        }
-        View::show("lot", $arr);
+
     }
 }
