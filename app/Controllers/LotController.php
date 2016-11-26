64fe1ce43b5613c8488835301ead292e8194a724
vova177 <1kaapaa@gmail.com>
2016-11-26 16:55:10 +0200

    commit lot to db from ui

--- app/Controllers/LotController.php
+++ app/Controllers/LotController.php
@@ -8,38 +8,51 @@ use App\Models\LotPhoto;
 use App\Controllers\Controller;
 use App\Database\DB;
 use App\Framework\View;
+use App\Services\Validator\Validator;
 
 class LotController
 {
     public function showLot(){
-        if(isset($_GET['lot_id']))
-        {
-            $lot = Lot::get($_GET['lot_id']);
-            $lastBet=DB::select("SELECT * FROM `bets` WHERE `lot_id`='{$_GET['lot_id']}'");
-        }
-        $maxPrice=isset($lastBet[0]['price']) ? $lastBet[0]['price'] : 0;
+        $lot_id=isset($_GET['lot_id']) ? $_GET['lot_id'] : 0;
         $user=isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 0;
         $bet=isset($_POST['bet']) ? (int)$_POST['bet'] : 0;
-        $maxPrice=$bet >$maxPrice ?  $bet : $maxPrice;
+        if($lot_id)
+        {
+            $lot = Lot::get($lot_id);
+            $lastBet=DB::select("SELECT MAX(`price`) as maxBet FROM `bets` WHERE `lot_id`='{$lot_id}'");
+        }
+        $maxBet=isset($lastBet[0]['maxBet']) ? (int)$lastBet[0]['maxBet'] : 0;
+        $bet= ($bet>$maxBet) ?  $bet : $maxBet;
 
-            if($user && $bet) {
-                $Bet=new Bet();
-                $Bet->setPrice($bet);
-                $Bet->setUser_id($_SESSION['user_id']);
-                $Bet->setLot_id($lot[0]->id);
-                $Bet->setCreated_at(date('Y-m-d H:i:s'));
-                $Bet->save();
-                if(isset($lastBet[0]['id']))
-                DB::delete("DELETE FROM `bets` WHERE `id`='{$lastBet[0]['id']}'");
-            }
-            else
-            {
+        $validate=new Validator();
+        $checkValid=$validate->validation(['bet'=>['numeric']],
+                                            ['bet'=>$bet]);
+
+        if($bet && $checkValid) {
+            if ($user) {
+                $time=date("Y-m-d H:i:s");
+                $userBet=DB::select("SELECT * FROM `bets` 
+                                      WHERE (`user_id`='{$user}' AND `lot_id`='{$lot_id}')");
+                if($userBet){
+                    DB::update("UPDATE `bets` 
+                                SET `price`='{$bet}', `created_at`='{$time}'
+                                WHERE(`user_id`='{$user}' AND `lot_id`='{$lot_id}')");
+                }else{
+                    $Bet=new Bet();
+                    $Bet->setPrice($bet);
+                    $Bet->setUser_id($user);
+                    $Bet->setLot_id($lot_id);
+                    $Bet->setCreated_at($time);
+                    $Bet->save();
+                }
+            } else {
                 header("location: /login");
                 exit();
             }
+        }
         View::show("lot", [
             'lot_page'=>$lot,
-            'maxPrice'=>$maxPrice
+            'maxBet'=>$bet
         ]);
     }
 }
--- app/Views/lot.html.php
+++ app/Views/lot.html.php
@@ -1,17 +1,20 @@
 <?php
 $action=(int)$data['lot_page'][0]->id;
     if(!empty($data['lot_page'][0])) {
-        foreach ($data['lot_page'] as $value){
-            echo  $value->name . "<br>";
-            echo  $value->description . "<br>";
-        }
-        echo "(type over bet as '".$data['maxPrice']."')";
-        echo '<form  action="/lot/?lot_id='.$action.'" method="POST">
+        foreach ($data['lot_page'] as $value)
+        {
+        ?>
+
+             <p> <?=$value->name?> </p>
+             <p> <?=$value->description?></p>
+<?php }?>
+
+<form  action="/lot/?lot_id=<?=$action?>" method="POST" >
+             <lavel>type over bet as '<?=$data['maxBet']?>'</lavel>
                  <input type="text" name="bet">
                 <input type="submit" value="Ok">
-              </form>';
-}
+              </form>
 
-
+<?php }?>
 
 
