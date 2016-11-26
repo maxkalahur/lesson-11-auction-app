2ed128a263ff7ee708a2a294b5a5aae4defa140e
vova177 <1kaapaa@gmail.com>
2016-11-26 15:46:38 +0200

    make bet

--- app/Controllers/LotController.php
+++ app/Controllers/LotController.php
@@ -15,21 +15,14 @@ class LotController
         if(isset($_GET['lot_id']))
         {
             $lot = Lot::get($_GET['lot_id']);
-            $lastBet=DB::select("SELECT * FROM `bets` WHERE lot_id='{$_GET['lot_id']}'");
+            $lastBet=DB::select("SELECT * FROM `bets` WHERE `lot_id`='{$_GET['lot_id']}'");
         }
         $maxPrice=isset($lastBet[0]['price']) ? $lastBet[0]['price'] : 0;
-        if(isset($_POST['bet']) && $_POST['bet']>$maxPrice)
-        {
-            $bet=(int)$_POST['bet'];
-            $maxPrice=$bet;
-        }
-        else
-            {
-                $bet=null;
-            }
-        if ($bet)
-        {
-            if(isset($_SESSION['user_id'])) {
+        $user=isset($_SESSION['user_id']) ? $_SESSION['user_id'] : 0;
+        $bet=isset($_POST['bet']) ? (int)$_POST['bet'] : 0;
+        $maxPrice=$bet >$maxPrice ?  $bet : $maxPrice;
+
+            if($user && $bet) {
                 $Bet=new Bet();
                 $Bet->setPrice($bet);
                 $Bet->setUser_id($_SESSION['user_id']);
@@ -44,7 +37,6 @@ class LotController
                 header("location: /login");
                 exit();
             }
-        }
         View::show("lot", [
             'lot_page'=>$lot,
             'maxPrice'=>$maxPrice
