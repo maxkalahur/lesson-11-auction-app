fe1ef4dcb64e5026144785286adb949dd79910a7
vova177 <1kaapaa@gmail.com>
2016-11-26 15:16:53 +0200

    commit bet from ui

--- app/Controllers/LotController.php
+++ app/Controllers/LotController.php
@@ -3,19 +3,51 @@
 namespace App\Controllers;
 
 use App\Models\Lot;
+use App\Models\Bet;
 use App\Models\LotPhoto;
 use App\Controllers\Controller;
+use App\Database\DB;
 use App\Framework\View;
 
 class LotController
 {
     public function showLot(){
-        if(isset($_GET['lot_id'])){
+        if(isset($_GET['lot_id']))
+        {
             $lot = Lot::get($_GET['lot_id']);
-            View::show("lot", ['lot_page'=>$lot]);
+            $lastBet=DB::select("SELECT * FROM `bets` WHERE lot_id='{$_GET['lot_id']}'");
+        }
+        $maxPrice=isset($lastBet[0]['price']) ? $lastBet[0]['price'] : 0;
+        if(isset($_POST['bet']) && $_POST['bet']>$maxPrice)
+        {
+            $bet=(int)$_POST['bet'];
+            $maxPrice=$bet;
+        }
+        else
+            {
+                $bet=null;
             }
-    }
-    public function withCategory(){
-
+        if ($bet)
+        {
+            if(isset($_SESSION['user_id'])) {
+                $Bet=new Bet();
+                $Bet->setPrice($bet);
+                $Bet->setUser_id($_SESSION['user_id']);
+                $Bet->setLot_id($lot[0]->id);
+                $Bet->setCreated_at(date('Y-m-d H:i:s'));
+                $Bet->save();
+                if(isset($lastBet[0]['id']))
+                DB::delete("DELETE FROM `bets` WHERE `id`='{$lastBet[0]['id']}'");
+            }
+            else
+            {
+                header("location: /login");
+                exit();
+            }
+        }
+        View::show("lot", [
+            'lot_page'=>$lot,
+            'maxPrice'=>$maxPrice
+        ]);
     }
 }
--- app/Views/lot.html.php
+++ app/Views/lot.html.php
@@ -1,9 +1,17 @@
 <?php
+$action=(int)$data['lot_page'][0]->id;
     if(!empty($data['lot_page'][0])) {
         foreach ($data['lot_page'] as $value){
             echo  $value->name . "<br>";
             echo  $value->description . "<br>";
         }
+        echo "(type over bet as '".$data['maxPrice']."')";
+        echo '<form  action="/lot/?lot_id='.$action.'" method="POST">
+                 <input type="text" name="bet">
+                <input type="submit" value="Ok">
+              </form>';
 }
+
+
 
 
